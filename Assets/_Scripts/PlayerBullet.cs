9213a0895e28704e65880f5c9d57ab0cbc803468
vherever <vherever@gmail.com>
2015-11-06 18:24:13 +0200

    shooting

---
+++ Assets/_Scripts/PlayerBullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBullet : MonoBehaviour {
+
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 8f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Get the bullet current position
+        Vector2 position = transform.position;
+
+        //compute the bullet's new position
+        position = new Vector2(position.x, position.y + speed * Time.deltaTime);
+
+        //Update the bullet's position
+        transform.position = position;
+	}
+}
